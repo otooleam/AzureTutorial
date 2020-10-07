8ed2c0aec626a79aeb5d8e60e7c9ab12bfc216e0
Faith Buehler <fabluebell@gmail.com>
2020-10-07 11:06:14 -0500

    Testing pipeline run.

--- SampleProgram/Program.cs
+++ SampleProgram/Program.cs
@@ -6,7 +6,7 @@ namespace SampleProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
