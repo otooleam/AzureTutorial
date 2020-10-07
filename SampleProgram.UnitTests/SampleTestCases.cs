fdbcd80a8ff03afa0d452483604961d7c60caea2
Faith Buehler <fabluebell@gmail.com>
2020-10-07 11:19:28 -0500

    Testing new pipeline.  Also, lots of junk code.  Sorry in advance.

---
+++ SampleProgram.UnitTests/SampleTestCases.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using SampleProgram;
+
+namespace SampleProgram.UnitTests
+{
+
+    [TestFixture]
+    class SampleTestCases
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Add()
+        {
+            var sample = new SampleCode();
+            Assert.AreEqual(31, sample.Add(20, 11));
+        }
+
+        [Test]
+        public void Sub()
+        {
+            SampleCode sample = new SampleCode();
+            Assert.AreEqual(10, sample.Sub(20, 10));
+        }
+    }
+}
---
+++ SampleProgram/SampleProgram/SampleCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleProgram
+{
+    public class SampleCode
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
